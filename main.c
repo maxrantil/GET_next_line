b4335a83258c74063b0bd43884fe0adafad48ea1
mrantil <mrantil@student.hive.fi>
2021-12-07 18:57:37 +0200

    happy day, just first return thats a problem now

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:00:51 by mrantil           #+#    #+#             */
-/*   Updated: 2021/12/07 15:29:31 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/12/07 18:56:49 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#define BUFF_SIZE 1111
+#define BUFF_SIZE 1
 
 int	ft_strlennl(char *str)
 {
@@ -28,7 +28,7 @@ int	ft_strlennl(char *str)
 	return (i + 1);
 }
 
-int get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
 	char			read_buf[BUFF_SIZE + 1];
 	char			*temp;
@@ -37,29 +37,27 @@ int get_next_line(const int fd, char **line)
 
 	if (!stat_buf)
 		stat_buf = ft_strnew(BUFF_SIZE);
-	if (ft_strchr(stat_buf, '\n') != NULL)  //check if \n is in stat_buf
+	if (ft_strchr(stat_buf, '\n') != NULL) //check if \n is in stat_buf
 	{
 		*line = ft_strsub(stat_buf, 0, ft_strlennl(stat_buf)); //cpy str before \n into  *line
 		if (ft_strlennl(ft_strchr(stat_buf, '\n'))) //check if there is a string in stat_buf after \n
-		{
-			temp = ft_strdup(ft_strchr(stat_buf, '\n' + 1));  // put leftover str into temp
-		}
+			temp = ft_strdup(ft_strchr(stat_buf, '\n' + 1)); // put leftover str into temp
 		free(stat_buf);
 	}
 	while (read(fd, read_buf, BUFF_SIZE))
 	{
-		if (ft_strchr(read_buf, '\n') == NULL) //check if buffer has newline - buffer no newline
+		if (ft_strchr(read_buf, '\n') == NULL && ft_strchr(stat_buf, '\0') != NULL) //check if buffer has newline - buffer no newline
 		{
 			check++;
 			if (check > 1)
 				stat_buf = ft_strjoin(stat_buf, ft_strdup(read_buf));
 			else
-				stat_buf = ft_strdup(read_buf);		//cpy buffer to static 	
+				stat_buf = ft_strdup(read_buf); //cpy buffer to static 	
 		}
 		else if (ft_strchr(read_buf, '\n') != NULL) //check if buffer has newline - buffer has newline 
 		{
-			stat_buf = ft_strjoin(stat_buf, ft_strsub(read_buf, 0, ft_strlennl(read_buf))); //(static+buffer before \n) (VARNING! look if work)
-			temp = ft_strdup(ft_strchr(read_buf, '\n') + 1);	//leftover from static into temp
+			stat_buf = ft_strjoin(stat_buf, ft_strsub(read_buf, 0, ft_strlennl(read_buf)) - 1); //(static+buffer before \n) (VARNING! look if work)
+			temp = ft_strdup(ft_strchr(read_buf, '\n') + 1); //leftover from static into temp
 			*line = ft_strdup(stat_buf);
 			ft_strdel(&stat_buf);
 			stat_buf = ft_strdup(temp);
@@ -68,6 +66,8 @@ int get_next_line(const int fd, char **line)
 			return (1);
 		}
 	}
+	*line = ft_strdup(stat_buf);
+	ft_strdel(&stat_buf);
 	return (0);
 }
 
@@ -77,13 +77,13 @@ int	main(int argc, char **argv)
 	char	*line;
 
 	fd = open(argv[1], O_RDONLY);
-	
 /*	if (get_next_line(fd, &line))
 		printf("%s", line);
 	if (get_next_line(fd, &line))
 		printf("%s", line);
 */	while (get_next_line(fd, &line))
 		printf("%s", line);
+		printf("%s", line);
 	return (0);
 }
-
+//first run returns one space too much and a nl?
