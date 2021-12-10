264d584bd96d80abcf0b624c2ba45ff9e70630ac
mrantil <mrantil@student.hive.fi>
2021-12-10 20:19:53 +0200

    almost done

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,35 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:00:51 by mrantil           #+#    #+#             */
-/*   Updated: 2021/12/09 19:32:49 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/12/10 20:10:44 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_strlennl(char *str)
+static int	ft_strclen(char *str, int c)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\n')
+	while (str[i] && str[i] != c)
+	{
 		i++;
-	return (i + 1);
+		if (str[i] == c)
+			return (i);
+	}
+	return (0);
+}
+
+static char	*ft_strfjoin(char *s1, const char *s2)
+{
+	char	*ret;
+
+	ret = ft_strjoin(s1, s2);
+	if (!ret)
+		return (NULL);
+	ft_strdel(&s1);
+	return (ret);
 }
 
 int	get_next_line(const int fd, char **line)
@@ -29,7 +44,6 @@ int	get_next_line(const int fd, char **line)
 	static char		*stat_buf[FD_MAX];
 	int				ret;
 
-	char			*temp2;
 	ret = 1;
 	if (!line || fd < 0 || fd > FD_MAX || BUFF_SIZE <= 0) //fd == 1 || fd == 2 - return (?)
 		return (-1);
@@ -37,8 +51,7 @@ int	get_next_line(const int fd, char **line)
 		stat_buf[fd] = ft_strnew(BUFF_SIZE);
 	if (ft_strchr(stat_buf[fd], '\n') != NULL) //check if \n is in stat_buf[fd]
 	{
-		ft_strdel(line);
-		*line = ft_strsub(stat_buf[fd], 0, ft_strlennl(stat_buf[fd]) - 1); //cpy str before \n into  *line
+		*line =	ft_strsub(stat_buf[fd], 0, ft_strclen(stat_buf[fd], '\n')); //cpy str before \n into  *line
 		if (ft_strchr(stat_buf[fd], '\n')) //check if there is a string in stat_buf[fd] after \n
 			temp = ft_strdup(ft_strchr(stat_buf[fd], '\n') + 1); // put leftover str into temp
 		ft_strdel(&stat_buf[fd]);
@@ -54,20 +67,12 @@ int	get_next_line(const int fd, char **line)
 			return (-1);
 		read_buf[ret] = '\0';
 		if (ft_strchr(read_buf, '\n') == NULL) //check if buffer has newline - buffer no newline
+			stat_buf[fd] = ft_strfjoin(stat_buf[fd], read_buf);
+		else // buffer has newline 
 		{
-			temp = ft_strdup(stat_buf[fd]);
-			ft_strdel(&stat_buf[fd]);
-			stat_buf[fd] = ft_strjoin(temp, read_buf);
+			temp = ft_strsub(read_buf, 0, ft_strclen(read_buf, '\n'));
+			stat_buf[fd] = ft_strfjoin(stat_buf[fd], temp);
 			ft_strdel(&temp);
-		}
-		else if (ft_strchr(read_buf, '\n') != NULL) //check if buffer has newline - buffer has newline 
-		{
-			temp = ft_strdup(stat_buf[fd]);
-			ft_strdel(&stat_buf[fd]);
-			temp2 = ft_strsub(read_buf, 0, ft_strlennl(read_buf) - 1);
-			stat_buf[fd] = ft_strjoin(temp, temp2); //(static+buffer before \n)
-			ft_strdel(&temp);
-			ft_strdel(&temp2);
 			temp = ft_strdup(ft_strchr(read_buf, '\n') + 1); //leftover from static into temp
 			*line = ft_strdup(stat_buf[fd]);
 			ft_strdel(&stat_buf[fd]);
@@ -76,9 +81,9 @@ int	get_next_line(const int fd, char **line)
 			return (1);
 		}
 	}
-	//if (ret == 0 && !line)	
-	//	return (1);
 	*line = ft_strdup(stat_buf[fd]);
 	ft_strdel(&stat_buf[fd]);
+	if (ft_strlen(*line) > 0)
+		return (1);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:00:51 by mrantil           #+#    #+#             */
-/*   Updated: 2021/12/09 19:21:14 by mrantil          ###   ########.fr       */
+/*   Updated: 2021/12/10 19:55:05 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <stdlib.h>
 
 # define FD_MAX 8192
-# define BUFF_SIZE 10000
+# define BUFF_SIZE 1000
 
 int	get_next_line(const int fd, char **line);
 
