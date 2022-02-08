775dc4cc25a2a354c01fe634ef87cf8d7e047dc9
Max <rantil@pm.me>
2022-02-08 22:22:18 +0200

    tests needs to be fixed more

--- tests/main_stout1lines.c
+++ tests/main_stout1lines.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_stout1lines.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrantil <mrantil@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:00:51 by mrantil           #+#    #+#             */
-/*   Updated: 2022/01/24 14:27:51 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/02/08 22:05:41 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,12 @@
 
 int	main()
 {
-	int		fd = 1;
+	int		fd = 0;
 	char	*line = NULL;
+	int		ret;
 
-	get_next_line(fd, &line);
-	printf("ret:%d		%s\n", fd, line);
+	ret = get_next_line(fd, &line);
+	printf("fd:%d	ret:%d	%s\n", fd, ret, line);
 //	ft_strdel(&line);
 //	system("leaks test_gnl");
 	return (0);
